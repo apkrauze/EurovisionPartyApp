12347254bf01cf15b081e60fbd229d216ba34279
Artur Krauze <apkrauze@gmail.com>
2022-02-28 22:26:07 +0100

    trying out the video as a welcome page- only mobile friendly

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import background from "./assets/background.mp4";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <video className="videoTag" autoPlay loop muted>
+        <source src={background} type="video/mp4" />
+      </video>
+    </div>
+  );
 }
 
 export default App;
