1fce398473fb5f608d07c6c0bc2c2753706489c1
Artur Krauze <apkrauze@gmail.com>
2022-03-17 23:35:24 +0100

    last commit for today, modified room.js with ideas of routing implemetation whats going to work with socket

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -1,13 +1,14 @@
 import io from "socket.io-client";
 import { useState } from "react";
-import Contestants from "../../screens/Contestants";
+//import Contestants from "../../screens/Contestants";
+import { Link } from "react-router-dom";
 //import { useNavigate } from "react-router-dom";
 
 const socket = io.connect("http://localhost:3001");
 
 const Room = () => {
   const [room, setRoom] = useState("");
-  const [showNextPage, setShowNextPage] = useState(false);
+  //const [showNextPage, setShowNextPage] = useState(false);
 
   const user = localStorage.getItem("username");
 
@@ -22,32 +23,31 @@ const Room = () => {
   const joinRoom = () => {
     if (room !== "") {
       socket.emit("join_room", { room, user });
-      setShowNextPage(true);
+      //setShowNextPage(true);
       //navigate("/contestants");
     }
   };
 
   return (
     <div>
-      {!showNextPage ? (
-        <div>
-          <h1>
-            Congratulations {user}! Invite your friends and vote together for
-            you favourite artists this year!
-          </h1>
-          <h3>Party choice</h3>
-          <input
-            type="text"
-            placeholder="Room..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Party</button>
-        </div>
-      ) : (
-        <Contestants socket={socket} room={room} />
-      )}
+      <div>
+        <h1>
+          Congratulations {user}! Invite your friends and vote together for you
+          favourite artists this year!
+        </h1>
+        <h3>Party choice</h3>
+        <input
+          type="text"
+          placeholder="Room..."
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        />
+        <Link onClick={joinRoom} to={`/contestants?room=${room}`}>
+          <button>Join A Party</button>
+        </Link>
+      </div>
+
       <br></br>
       <div>
         <button onClick={handleLogout}>Logout</button>
